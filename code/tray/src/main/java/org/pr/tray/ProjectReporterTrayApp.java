dd61c9791c222beedc2a3111061fc188cb81b8ce
jmadej <jmadej@o2.pl>
2011-02-21 19:51:26 +0100

    first commit

---
+++ code/tray/src/main/java/org/pr/tray/ProjectReporterTrayApp.java
@@ -0,0 +1,14 @@
+package org.pr.tray;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jacek
+ * Date: 20.02.11
+ * Time: 20:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProjectReporterTrayApp {
+    public static void main(String args[]) {
+
+    }
+}
